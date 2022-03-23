2263c1d216de74cc64336d3bd1f253aae12ced9a
juandavid <juandavidalvarez52@gmail.com>
2022-03-23 14:53:46 -0500

    user-get-create-endpoint

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,17 @@
-import { Column, PrimaryGeneratedColumn, Entity } from "typeorm";
+import {
+  Column, PrimaryGeneratedColumn, Entity, OneToMany,
+} from "typeorm";
+import Photo from "./Photo";
+
+enum RoleEnum{
+  admin,
+  developer,
+  designer,
+  project,
+  manager,
+  scrumMaster,
+  HR,
+}
 
 @Entity()
 export default class User {
@@ -11,9 +24,16 @@ export default class User {
   @Column()
     email: string;
 
-  @Column()
-    password: string;
+  @Column({ nullable:true })
+    password!: string;
 
-  @Column()
-    avatarImg: string;
+  @Column({ nullable:true })
+    avatarImg!: string;
+
+  @Column({ nullable:true, default: RoleEnum.developer })
+    role!: RoleEnum;
+
+  @OneToMany(() => Photo, photo => photo.user, { eager:true })
+    photos: Photo[];
+
 }
